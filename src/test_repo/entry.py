f43049f777d32779d2fe0e6c051cba6a08c4755b
Honglu Fan <honglu2875@gmail.com>
2023-04-26 15:29:45 +0000

    a

---
+++ src/test_repo/entry.py
@@ -0,0 +1,15 @@
+import verisynth
+
+
+@verisynth.completion()
+def palindrome(original: str) -> bool:
+    """
+    Return True if `original` is a palindrome (case sensitive), False otherwise.
+    """
+
+
+@verisynth.completion()
+def sum_of_digits(n: int) -> int:
+    """
+    Return the sum of the digits of `n`.
+    """
